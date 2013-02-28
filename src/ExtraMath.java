620d73f8e43fc853f247ebdda40ffa66a17a3bed
Joost Verplancke <joost.verplancke@gmail.com>
2013-02-28 19:18:46 +0100

    Begin voor Extra Math

---
+++ src/ExtraMath.java
@@ -0,0 +1,43 @@
+/**
+ * We want to circumvene the fact that, even with double precision, numbers can't be represented
+ * correctly in a computer, which might give problems when comparing them.
+ * @author Joost Verplancke
+ * @version 1.0
+ *
+ */
+public abstract class ExtraMath {
+	
+	public static double errorMarge = 2e-50;
+	
+	
+	/**
+	 * 
+	 * @param firstdouble
+	 * @param secondouble
+	 * @return We check if the given doubles are close enough together to consider them equal.
+	 * 			| result = Math.abs(firstdouble - seconddouble) < getErrorMarge() || 
+							Math.abs(seconddouble - firstdouble) < getErrorMarge();
+	 */
+	public static boolean compare(double firstdouble, double seconddouble){
+		boolean result = Math.abs(firstdouble - seconddouble) < getErrorMarge() || 
+							Math.abs(seconddouble - firstdouble) < getErrorMarge();
+		return result;
+	}
+
+
+	private static double getErrorMarge() {
+		return errorMarge;
+	}
+	
+	public static void setErrorMarge(double newMarge) throws IllegalArgumentException{
+		if(!isValidErrorMarge(newMarge)) throw new IllegalArgumentException();
+			errorMarge = newMarge;
+	}
+	
+	private static boolean isValidErrorMarge(double marge){
+		boolean result = !Double.isNaN(marge);
+		result = result && marge > 0;
+		result = result && marge < 2e-10;
+		return result;
+	}
+}
